161b998ad22d94f4561f1fc80672def563778459
NoHeartPen <NoHeartPen@outlook.com>
2022-07-30 10:04:12 +0800

    update：删除词条中非假名结尾的词条

---
+++ v3/python/test/v3_delete_index_duplicate_lines.py
@@ -0,0 +1,14 @@
+import os
+
+ProcessPath = os.getcwd()
+
+with open(r'temp.txt', 'r', encoding='utf-8') as f, open(r'save.txt', 'w', encoding='utf-8')as s:
+    for line in f:
+        if line != '\n\n': # 跳过空行
+            line = line.replace('\n', '')
+            if 12353 < ord(line[-1]) < 12435:
+                s.write(line+'\n') # 只保留以假名结尾的词条
+            else:
+                continue
+        else:
+            print(line)
