b9136d9ac8af591dc7d6a1894be2437f369cf735
noheartpen <3104408599@qq.com>
2024-03-23 21:10:26 +0900

    refactor: 模块化 preprocess

---
+++ v3/python/test/preprocess.py
@@ -0,0 +1,221 @@
+"""Preprocess the input text."""
+
+import re
+
+
+def del_word_ruby(input_text: str) -> str:
+    """Removes ruby character from the input text.
+        移除假名注音
+
+    Args:
+        input (str) : A string contains ruby.
+
+    Returns:
+        str: The text with converted ruby character.
+    """
+    # 通过检查注音符号前的字符串是否是汉字，判断是否是在为汉字注音
+    # 汉字的 Unicode 编码范围请参考下面的链接
+    # https://www.unicode.org/charts/
+    reg = re.compile(
+        r"""(?P<cjk_unified_ideographs>[\u4E00-\u9FFF])|
+            (?P<extension_a>[\u3400-\u4DBF])|
+            (?P<extension_b>[\u20000-\u2A6DF])|
+            (?P<extension_c>[\u2A700-\u2B738])|
+            (?P<extension_d>[\u2B740-\u2B81D])|
+            (?P<extension_e>[\u2B820-\u2CEA1])|
+            (?P<extension_f>[\u2CEB0-\u2EBE0])|
+            (?P<extension_g>[\u30000-\u23134A])|
+            (?P<extension_h>[\u31350-\u323AF])|
+            (?P<extension_i>[\u2EBF0-\u2EE5F])(\(|（|《)(.*?)
+        """
+    )
+    if reg.search(input_text) is None:
+        return input_text
+
+    reg = re.compile(r"(\(|（|《)[\u3040-\u309f]*?(\)|）|》)")
+    replacement = r""
+    output_text = re.sub(reg, replacement, input_text)
+    return output_text
+
+
+def convert_kata_to_hira(input_text: str) -> str:
+    """Convert katakana to hiragana in the given text.
+    将片假名转为平假名
+
+    Args:
+        input_text (str): A String containing the katakana.
+
+    Returns:
+        str: The text with katakana converted to hiragana.
+    """
+    output_text = ""
+    for gana in input_text:
+        # 关于取值范围，请阅读下面的链接
+        # Read url for why the condition is 12448 and 13543
+        # https://www.unicode.org/charts/PDF/U30A0.pdf
+        gana_code = int(ord(gana))
+        if 12448 < gana_code < 12543:
+            hira = chr(gana_code - 96)
+            output_text = output_text + hira
+        else:
+            output_text = output_text + gana
+    return output_text
+
+
+def convert_repe_single_sign(input_text: str) -> str:
+    """Converts a repeated single sign (々 or 〻 or ゝ or ヽ) in the given text.
+        移除单字符重复符号々、〻、ゝ、ヽ
+
+    Args:
+        input (str): A string containing the repeated single sign.
+
+    Returns:
+        str: The text with converted repeated single sign.
+    """
+    reg = r"^(.*?)(々|〻|ゝ|ヽ)(.*?)$"
+    match = re.match(reg, input_text)
+    if not match:
+        return input_text
+
+    i = 0
+    output_text = ""
+    while i < len(input_text):
+        if i != 0:
+            if input_text[i] in "々〻ゝヽ":
+                output_text += input_text[i - 1]
+            else:
+                output_text += input_text[i]
+        else:
+            # 当"々"等符号位于第一个位置时，不做任何处理，例：々段
+            output_text += input_text[i]
+        i += 1
+    return output_text
+
+
+def convert_repe_single_daku_sign(input_text: str) -> str:
+    """Converts a repeated single daku sign (ヾ or ゞ) in the given text.
+        移除单字符浊音符号ヾ、ゞ
+
+    Args:
+        input_text (str): A string containing the repeated single daku sign.
+
+    Returns:
+        str: The text with converted repeated single daku sign.
+    """
+    reg = r"^(.*?)(\w{1})(ヾ|ゞ)(.*?)$"
+    match = re.match(reg, input_text)
+    if not match:
+        return input_text
+
+    # 匹配单字符浊音符前的字符串（不包括单字符浊音符前的第一个字符串）
+    pre_text = match.group(1)
+    # 计算单字符浊音符前的第一个字符串
+    char = match.group(2)
+    new_char = chr(ord(char) + 1)
+    # 匹配单字符浊音后的所有字符串
+    post_text = match.group(4)
+
+    output_text = pre_text + char + new_char + post_text
+    return output_text
+
+
+def convert_repe_double_sign(input_text: str) -> str:
+    """Converts repeated double sign in the given text.
+        移除多字符重复符号〳〵、／＼、〱
+
+    Args:
+        input_text (str): The text containing the repeated double sign.
+
+    Returns:
+        str: The text with converted repeated double sign.
+    """
+    match = re.match(r"^(.*?)(〳〵|／＼|〱)$", input_text)
+
+    if not match:
+        return input_text
+
+    # 匹配多字符重复符号前的字符串
+    pre_input_text = match.group(1)
+
+    output_text = pre_input_text + pre_input_text
+    return output_text
+
+
+def convert_repe_double_daku_sign(input_text: str) -> str:
+    """Convert a repeated double daku sign ( 〴〵, ／″＼) in the given text.
+        移除多字符浊音符号 〴〵、／″＼
+
+    Args:
+        input (str): A String containing the repeated double daku sign.
+
+    Returns:
+        str: The text with converted repeated double daku sign.
+    """
+    match = re.match(r"^(.*?)(〴〵|／″＼)(.*?)$", input_text)
+
+    if not match:
+        return input_text
+
+    # 匹配多字符浊音符号前的字符串
+    pre_input_text = match.group(1)
+    # 匹配多字符浊音符号后的字符串
+    post_input_text = match.group(3)
+
+    if re.search(r"[^\u3040-\u30ff]", pre_input_text) is not None:
+        # 如果多字符浊音符号前的字符串中不止汉字
+        # 比如像「代わる〴〵」这样，同时含有汉字和假名
+        # 那么拼接多字符浊音符号前的部分然后输出拼接后的字符串，例：「代わる代わる」
+        output_text = pre_input_text + pre_input_text + post_input_text
+    elif re.search(r"([\u3040-\u30ff]{1})(.*?)", pre_input_text) is not None:
+        # 提取多字符浊音符号前的字符串的第一个假名并计算出对应的浊音假名
+        # 拼接后输出拼接后的字符串
+        daku_character = chr(int(ord(pre_input_text[0])) + 1)
+        new_pre_input_text = daku_character + pre_input_text[1:]
+        output_text = pre_input_text + new_pre_input_text + post_input_text
+    else:
+        print(f"input_text is: {input_text}")
+        return input_text
+    return output_text
+
+
+def del_ocr_error(input_text: str) -> str:
+    """Removes OCR errors from the input text.
+        移除 OCR 易识别错误的字符
+
+    Args:
+        input (str): a string containing OCR-detected text with spaces and newlines.
+
+    Returns:
+        str: a processed string with spaces and newlines removed.
+    """
+    input_text = input_text.replace(" ", "")
+    output_text = input_text.replace("\n", "")
+    output_text = input_text.replace("\r\n", "")
+    return output_text
+
+
+def preprocess(input_text: str) -> str:
+    """Preprocess the input text.
+        预处理输入文本
+
+    Args:
+        input_text (str): The input text.
+
+    Returns:
+        str: The processed text.
+    """
+    input_text = del_ocr_error(input_text)
+    if "(" in input_text:
+        input_text = del_word_ruby(input_text)
+    # 转换片假名书写的单词
+    if re.search(r"^[\u30a0-\u30ff]*?$", input_text) is not None:
+        input_text = convert_kata_to_hira(input_text)
+    if re.search(r"(\w{1})(々|〻|ゝ|ヽ)", input_text) is not None:
+        input_text = convert_repe_single_sign(input_text)
+    if re.search(r"^(.*?)(\w{1})(ヾ|ゞ)(.*?)$", input_text) is not None:
+        input_text = convert_repe_single_daku_sign(input_text)
+    if re.search(r"^(\w{2})(〳〵|／＼)(.*?)$", input_text) is not None:
+        input_text = convert_repe_double_sign(input_text)
+    if re.search(r"^(.*?)(〴〵|／″＼)(.*?)$", input_text) is not None:
+        input_text = convert_repe_double_daku_sign(input_text)
+    return input_text
--- v3/python/test/testmain.py
+++ v3/python/test/testmain.py
@@ -1,7 +1,15 @@
 """单元测试框架 """
 
 import unittest
-import main
+from preprocess import (
+    del_word_ruby,
+    convert_kata_to_hira,
+    convert_repe_single_sign,
+    convert_repe_single_daku_sign,
+    convert_repe_double_sign,
+    convert_repe_double_daku_sign,
+    del_ocr_error,
+)
 
 
 class TestMain(unittest.TestCase):
@@ -13,66 +21,66 @@ class TestMain(unittest.TestCase):
     def test_del_word_ruby(self):
         """移除假名注音"""
         # ()
-        self.assertEqual("始む", main.del_word_ruby("始(はじ)む"))
+        self.assertEqual("始む", del_word_ruby("始(はじ)む"))
         # （）
-        self.assertEqual("始む", main.del_word_ruby("始（はじ）む"))
+        self.assertEqual("始む", del_word_ruby("始（はじ）む"))
         # 《》
         # 其れはまだ人々が「愚《おろか》」と云う貴い徳を持って居て、世の中が今のように激しく軋《きし》み合わない時分であった。
         # https://www.aozora.gr.jp/cards/001383/card56641.html
-        self.assertEqual("「愚」", main.del_word_ruby("「愚《おろか》」"))
-        self.assertEqual("軋み合わない", main.del_word_ruby("軋《きし》み合わない"))
+        self.assertEqual("「愚」", del_word_ruby("「愚《おろか》」"))
+        self.assertEqual("軋み合わない", del_word_ruby("軋《きし》み合わない"))
         # 非正常输入测试
         # https://kotobank.jp/word/%E3%81%BC%E3%82%84%E3%81%BC%E3%82%84-631393
         # ぼやぼや（と）しないでさっさと荷物を運べ
-        self.assertEqual("ぼやぼや（と）", main.del_word_ruby("ぼやぼや（と）"))
+        self.assertEqual("ぼやぼや（と）", del_word_ruby("ぼやぼや（と）"))
 
     def test_convert_kata_to_hira(self):
         """将片假名转为平假名"""
-        self.assertEqual("くま", main.convert_kata_to_hira("クマ"))
-        self.assertEqual("ちょこちょこ", main.convert_kata_to_hira("チョコチョコ"))
+        self.assertEqual("くま", convert_kata_to_hira("クマ"))
+        self.assertEqual("ちょこちょこ", convert_kata_to_hira("チョコチョコ"))
         # アニメ「ヴァイオレット・エヴァーガーデン」：会えない日が続くと　胸がグッと重くなったりしないか？
-        self.assertEqual("ぐっと", main.convert_kata_to_hira("グッと"))
+        self.assertEqual("ぐっと", convert_kata_to_hira("グッと"))
 
     def test_convert_repe_single_sign(self):
         """单字符重复符号"""
         # https://ja.wikipedia.org/wiki/%E8%B8%8A%E3%82%8A%E5%AD%97
         # 々
-        self.assertEqual("正正堂堂", main.convert_repe_single_sign("正々堂々"))
-        self.assertEqual("段段", main.convert_repe_single_sign("段々"))
-        self.assertEqual("赤裸裸", main.convert_repe_single_sign("赤裸々"))
-        self.assertEqual("告別式式場", main.convert_repe_single_sign("告別式々場"))
+        self.assertEqual("正正堂堂", convert_repe_single_sign("正々堂々"))
+        self.assertEqual("段段", convert_repe_single_sign("段々"))
+        self.assertEqual("赤裸裸", convert_repe_single_sign("赤裸々"))
+        self.assertEqual("告別式式場", convert_repe_single_sign("告別式々場"))
 
         # 非正常输入测试
-        self.assertEqual("正正堂", main.convert_repe_single_sign("正々堂"))
-        self.assertEqual("々段", main.convert_repe_single_sign("々段"))
-        self.assertEqual("々堂堂", main.convert_repe_single_sign("々堂々"))
+        self.assertEqual("正正堂", convert_repe_single_sign("正々堂"))
+        self.assertEqual("々段", convert_repe_single_sign("々段"))
+        self.assertEqual("々堂堂", convert_repe_single_sign("々堂々"))
         # TODO 极少数特例
-        # self.assertEqual("複複複線", main.convert_repe_single_sign("複々々線"))
-        # self.assertEqual("部分部分", main.convert_repe_single_sign("部分々々"))
+        # self.assertEqual("複複複線", convert_repe_single_sign("複々々線"))
+        # self.assertEqual("部分部分", convert_repe_single_sign("部分々々"))
         # TODO 古く（奈良時代）は記法が異なり
-        # self.assertEqual("部分部分", main.convert_repe_single_sign("部々分々"))
+        # self.assertEqual("部分部分", convert_repe_single_sign("部々分々"))
 
         # 〻 現代では「〻」は「々」と書き換えられ、主に縦書きの文章に用いる。
-        self.assertEqual("屡屡", main.convert_repe_single_sign("屡〻"))
+        self.assertEqual("屡屡", convert_repe_single_sign("屡〻"))
 
         # ゝ 平仮名繰返し記号
-        self.assertEqual("ここ", main.convert_repe_single_sign("こゝ"))
-        self.assertEqual("こころ", main.convert_repe_single_sign("こゝろ"))
-        self.assertEqual("わななかした", main.convert_repe_single_sign("わなゝかした"))
+        self.assertEqual("ここ", convert_repe_single_sign("こゝ"))
+        self.assertEqual("こころ", convert_repe_single_sign("こゝろ"))
+        self.assertEqual("わななかした", convert_repe_single_sign("わなゝかした"))
 
         # ヽ 片仮名繰返し記号
-        self.assertEqual("ハハヽヽ", main.convert_repe_single_sign("ハヽヽヽ"))
+        self.assertEqual("ハハヽヽ", convert_repe_single_sign("ハヽヽヽ"))
         # 曾ては、妣 （ ハヽ ） が国として、恋慕の思ひをよせた此国は、現実の悦楽に満ちた楽土として、見かはすばかりに変つて了うた。
         # https://www.aozora.gr.jp/cards/000933/files/13212_14465.html
-        self.assertEqual("ハハ", main.convert_repe_single_sign("ハヽ"))
+        self.assertEqual("ハハ", convert_repe_single_sign("ハヽ"))
 
     def test_convert_repe_single_daku_sign(self):
         """单字符浊音重复符号"""
         # https://ja.wikipedia.org/wiki/%E8%B8%8A%E3%82%8A%E5%AD%97#%E3%82%9D%E3%81%A8%E3%83%BD%EF%BC%88%E4%B8%80%E3%81%AE%E5%AD%97%E7%82%B9%EF%BC%89
-        self.assertEqual("ただ", main.convert_repe_single_daku_sign("たゞ"))
-        self.assertEqual("みすず飴", main.convert_repe_single_daku_sign("みすゞ飴"))
+        self.assertEqual("ただ", convert_repe_single_daku_sign("たゞ"))
+        self.assertEqual("みすず飴", convert_repe_single_daku_sign("みすゞ飴"))
         # FIXME 注意这个字符的定义：代表一个浊音假名，当前浊音符前的第一个假名就是浊音时，直接拼接返回即可
-        # self.assertEqual("ぶぶ漬け", main.convert_repe_single_daku_sign("ぶゞ漬け"))
+        # self.assertEqual("ぶぶ漬け", convert_repe_single_daku_sign("ぶゞ漬け"))
 
     def test_convert_repeated_double_sign(self):
         """多字符重复符号"""
@@ -82,51 +90,43 @@ class TestMain(unittest.TestCase):
         # 青空文庫では「／＼」を使っている。
         # https://www.aozora.gr.jp/cards/001383/files/56641_59496.html
         # 時々両国で催される刺青会では参会者おの／＼肌を叩いて、互に奇抜な意匠を誇り合い、評しあった。
-        self.assertEqual("おのおの", main.convert_repe_double_sign("おの／＼"))
+        self.assertEqual("おのおの", convert_repe_double_sign("おの／＼"))
         # 〳〵
-        self.assertEqual("おのおの", main.convert_repe_double_sign("おの〳〵"))
-        self.assertEqual(
-            "くり返しくり返し", main.convert_repe_double_sign("くり返し〳〵")
-        )
+        self.assertEqual("おのおの", convert_repe_double_sign("おの〳〵"))
+        self.assertEqual("くり返しくり返し", convert_repe_double_sign("くり返し〳〵"))
         # 〱
-        self.assertEqual("見る見る", main.convert_repe_double_sign("見る〱"))
+        self.assertEqual("見る見る", convert_repe_double_sign("見る〱"))
         # https://ja.wikipedia.org/wiki/%E8%B8%8A%E3%82%8A%E5%AD%97#%E3%80%B1%EF%BC%88%E3%81%8F%E3%81%AE%E5%AD%97%E7%82%B9%EF%BC%89
-        self.assertEqual(
-            "どうしてどうして", main.convert_repe_double_sign("どうして〱")
-        )
+        self.assertEqual("どうしてどうして", convert_repe_double_sign("どうして〱"))
         # 非正常输入测试
-        self.assertEqual("おの／＼肌", main.convert_repe_double_sign("おの／＼肌"))
+        self.assertEqual("おの／＼肌", convert_repe_double_sign("おの／＼肌"))
 
     def test_convert_repeated_double_daku_sign(self):
         """多字符浊音符号"""
         # https://ja.wikisource.org/wiki/%E3%81%8F%E3%82%8A%E3%81%8B%E3%81%B8%E3%81%97%E7%AC%A6%E5%8F%B7%E3%81%AE%E4%BD%BF%E3%81%B2%E6%96%B9
-        self.assertEqual("散り散り", main.convert_repe_double_daku_sign("散り〴〵"))
-        self.assertEqual(
-            "代わる代わる", main.convert_repe_double_daku_sign("代わる〴〵")
-        )
+        self.assertEqual("散り散り", convert_repe_double_daku_sign("散り〴〵"))
+        self.assertEqual("代わる代わる", convert_repe_double_daku_sign("代わる〴〵"))
         # 请注意，这个样例并非来自真实的使用场景，仅用作测试
-        self.assertEqual(
-            "かわるがわる", main.convert_repe_double_daku_sign("かわる〴〵")
-        )
+        self.assertEqual("かわるがわる", convert_repe_double_daku_sign("かわる〴〵"))
         # https://www.aozora.gr.jp/cards/001383/files/56641_59496.html
         # 彼は今始めて女の妙相みょうそうをしみ／″＼味わう事が出来た。
-        self.assertEqual("しみじみ", main.convert_repe_double_daku_sign("しみ／″＼"))
+        self.assertEqual("しみじみ", convert_repe_double_daku_sign("しみ／″＼"))
         # 其の瞳は夕月の光を増すように、だん／＼と輝いて男の顔に照った。
         # TODO　以下のURLでは、
         # https://ja.wikipedia.org/wiki/%E8%B8%8A%E3%82%8A%E5%AD%97#%E3%80%B1%EF%BC%88%E3%81%8F%E3%81%AE%E5%AD%97%E7%82%B9%EF%BC%89
         # 濁点の付く文字を繰り返す場合は、濁点の付いていない「くの字点」を用いる場合と、濁点の付いている「くの字点」を用いる場合がある。
         # 濁点の付く文字を繰り返すが、繰り返し箇所は濁点がつかない場合は、濁点の付いていない「くの字点」を用いる（擬音などでは少ないが児童向け文学などで漢字を仮名表記する場合に用いられる）。
-        # self.assertEqual("だんだん", main.convert_repe_double_daku_sign("だん／＼"))
-        # self.assertEqual("だんだん", main.convert_repe_double_daku_sign("だん／″＼"))
+        # self.assertEqual("だんだん", convert_repe_double_daku_sign("だん／＼"))
+        # self.assertEqual("だんだん", convert_repe_double_daku_sign("だん／″＼"))
 
     def test_del_ocr_error(self):
         """注意空格往往不止一个"""
-        self.assertEqual("食べた", main.del_ocr_error(" 食べた"))
-        self.assertEqual("食べた", main.del_ocr_error("食 べた"))
-        self.assertEqual("食べた", main.del_ocr_error("食べた "))
-        self.assertEqual("食べた", main.del_ocr_error(" 食べた "))
-        self.assertEqual("食べた", main.del_ocr_error(" 食 べ た "))
-        self.assertEqual("", main.del_ocr_error(" "))
+        self.assertEqual("食べた", del_ocr_error(" 食べた"))
+        self.assertEqual("食べた", del_ocr_error("食 べた"))
+        self.assertEqual("食べた", del_ocr_error("食べた "))
+        self.assertEqual("食べた", del_ocr_error(" 食べた "))
+        self.assertEqual("食べた", del_ocr_error(" 食 べ た "))
+        self.assertEqual("", del_ocr_error(" "))
 
 
 if __name__ == "__main__":
