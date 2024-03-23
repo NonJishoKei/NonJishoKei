c23427765cf8553939e4893a016971b0054271b9
noheartpen <3104408599@qq.com>
2024-03-23 22:24:29 +0900

    refactor: search_index

--- v3/python/test/main.py
+++ v3/python/test/main.py
@@ -1,15 +1,14 @@
 """convert a nonjishokei to a jishokei"""
 
 import os
-from preprocess import preprocess
-import time
+import json
+from typing import Dict, List
+from textwrap import dedent
 
-START_TIME = time.perf_counter()
 
-
-def convert_v5(input_text: str) -> str:
+def convert_v5(input_text: str, input_stem: str, input_last_letter: str) -> str:
     """convert a v5 verb conjugation to basic form.
-        还原五段动词的活用变形
+        还原五段动词的活用变形，请注意，该函数只处理词尾假名来自五段动词活用的假名。
 
     Args:
         input_text (str): A String containing the conjugation.
@@ -17,55 +16,68 @@ def convert_v5(input_text: str) -> str:
     Returns:
         str: The text with conjugation converted to the basic form.
     """
+    if input_text != input_stem + input_last_letter:
+        raise ValueError(
+            dedent(
+                f"""input_text is {input_text},
+            but input_stem is {input_stem}, input_last_letter is {input_last_letter},
+            you may edited source in a wrong way,
+            or you can report on you can report on github: 
+            https://github.com/NoHeartPen/NonJishoKei"""
+            )
+        )
+
     v5_nonjishokei_last_letter = set(
         "えおかがきぎけげこごさしせそたちてとなにねのばびべぼまみめもらりれろわ"
     )
-    if INPUT_LAST_LETTER not in v5_nonjishokei_last_letter:
-        raise ValueError(
-            f"""{input_text} is not v5,
-            you can report on github: https://github.com/NoHeartPen/NonJishoKei"""
-        )
-    if INPUT_LAST_LETTER in "がぎげご":
-        jishokei = input_text[0:-1] + "ぐ"
-    elif INPUT_LAST_LETTER == "と":
-        jishokei = input_text[0:-1] + "つ"
-    elif INPUT_LAST_LETTER == "ば":
-        jishokei = input_text[0:-1] + "ぶ"
-    elif INPUT_LAST_LETTER == "わ":
-        jishokei = input_text[0:-1] + "う"
+    if input_last_letter not in v5_nonjishokei_last_letter:
+        if input_last_letter in "い":
+            raise ValueError(
+                dedent(
+                    f"""{input_text} is v5, but you shouldn't use this function,
+                        you can report on github: 
+                        https://github.com/NoHeartPen/NonJishoKei"""
+                )
+            )
+        else:
+            raise ValueError(
+                dedent(
+                    f"""{input_text} is not v5,
+                        you can report on github: 
+                        https://github.com/NoHeartPen/NonJishoKei"""
+                )
+            )
+    if input_last_letter in "がぎげご":
+        jishokei = input_stem + "ぐ"
+    elif input_last_letter == "と":
+        jishokei = input_stem + "つ"
+    elif input_last_letter == "ば":
+        jishokei = input_stem + "ぶ"
+    elif input_last_letter == "わ":
+        jishokei = input_stem + "う"
     else:
         jishokei_dic = {}
         v5_jishokei_last_letter = set("うくすつぬぶむる")
         # TODO fix
         for i in v5_jishokei_last_letter:
             # 计算输入的假名与词尾原型假名之间的距离
-            jishokei_dic[abs(ord(i) - ord(INPUT_LAST_LETTER))] = i
+            jishokei_dic[abs(ord(i) - ord(input_last_letter))] = i
         jishokei = input_text[0:-1] + jishokei_dic.get(
             min(jishokei_dic.keys()), "无法判断该五段假名的原型"
         )
     return jishokei
 
 
-index_set = set()
-orthography_set = set()
-orthography_dict = dict()
+orthography_dict: Dict[str, list[str]] = dict()
 CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
-with open(os.path.join(CURRENT_PATH, "v3_index.txt"), "r", encoding="utf-8") as f:
-    for item in f.readlines():
-        item = item.replace("\n", "")
-        if "\t" in item:
-            orthography = item.split("\t")[0]
-            orthography_set.add(orthography)
-            index_set.add(orthography)
-            orthography_dict[orthography] = item.split("\t")[1]
-        else:
-            index_set.add(item)
+with open(os.path.join(CURRENT_PATH, "index_v3.json"), "r", encoding="utf-8") as f:
+    orthography_dict = json.loads(f.read())
 
 
-def convert_orthography(input_text: str) -> str:
+def convert_orthography(input_text: str) -> list:
     """convert input text to the form of a word that appears as an entry in a dictionary,
         for example, convert【気づく】to【気付く】
-        通过查询消除假名书写造成的非辞書型，比如【気づく】和【気付く】
+        通过查询确认推导结果是否正确，同时消除假名书写造成的非辞書型，比如【気づく】和【気付く】
 
     Args:
         input_text (str): a form of a word that will not appear as an entry in a dictionary
@@ -73,42 +85,19 @@ def convert_orthography(input_text: str) -> str:
     Returns:
         str: the form of a word that appears as an entry in a dictionary
     """
-    if input_text in orthography_set:
-        search_result = orthography_dict.get(input_text)
-        return search_result
+    if input_text in orthography_dict:
+        return orthography_dict[input_text]
     else:
-        return input_text
+        return [input_text]
 
 
-def search_index(input_text: str) -> str:
-    """confirm whether the derivation results are correct by querying.
-        通过查询确认推导结果是否正确
-
-    Args:
-        input_text (str): the results deduced by the program are not necessarily real words.
-
-    Returns:
-        str: a word that appears as an entry in a dictionary
-    """
-    # TODO 与 convert_orthography 函数合并
-    if input_text in index_set:
-        # TODO not use global
-        global SearchResult
-        SearchResult = input_text
-        process_output_list.append(SearchResult)
-        return SearchResult
-    else:
-        # TODO this looks like a bug
-        SearchResult = INPUT_TEXT + "无该索引"
-        return False
-
-
-def convert_v5_need_once(input_text: str) -> str:
+def convert_v5_need_once(input_stem: str, input_last_letter: str) -> str:
     """convert input text that can only come from a v5 verb
         还原只可能来自一个五段动词的动词活用的词尾假名
 
     Args:
-        input_text (str): A String containing the conjugation.
+        input_stem (str): A String containing the conjugation.
+        input_last_letter (str): A String containing the conjugation.
 
     Raises:
         ValueError: the input text cannot be converted by this function,
@@ -117,26 +106,26 @@ def convert_v5_need_once(input_text: str) -> str:
     Returns:
         str: The text with conjugation converted to the basic form.
     """
-    if INPUT_LAST_LETTER in "わえお":
-        process_result = input_text[0:-1] + "う"
-    elif INPUT_LAST_LETTER in "かきけこ":
-        process_result = input_text[0:-1] + "く"
-    elif INPUT_LAST_LETTER in "がぎげご":
-        process_result = input_text[0:-1] + "ぐ"
-    elif INPUT_LAST_LETTER in "しせ":
-        process_result = input_text[0:-1] + "す"
-    elif INPUT_LAST_LETTER in "にねの":
-        process_result = input_text[0:-1] + "ぬ"
-    elif INPUT_LAST_LETTER in "ばびべぼ":
-        process_result = input_text[0:-1] + "ぶ"
-    elif INPUT_LAST_LETTER in "めも":
-        process_result = input_text[0:-1] + "む"
-    elif INPUT_LAST_LETTER in "り":
-        process_result = input_text[0:-1] + "る"
+    if input_last_letter in "わえお":
+        process_result = input_stem + "う"
+    elif input_last_letter in "かきけこ":
+        process_result = input_stem + "く"
+    elif input_last_letter in "がぎげご":
+        process_result = input_stem + "ぐ"
+    elif input_last_letter in "しせ":
+        process_result = input_stem + "す"
+    elif input_last_letter in "にねの":
+        process_result = input_stem + "ぬ"
+    elif input_last_letter in "ばびべぼ":
+        process_result = input_stem + "ぶ"
+    elif input_last_letter in "めも":
+        process_result = input_stem + "む"
+    elif input_last_letter in "り":
+        process_result = input_stem + "る"
     else:
-        process_result = input_text
+        process_result = input_stem + input_last_letter
         raise ValueError(
-            f"""{input_text} shouldn't converted by this function,
+            f"""{process_result} shouldn't converted by this function,
             you can report on github: https://github.com/NoHeartPen/NonJishoKei"""
         )
     return process_result
@@ -145,7 +134,7 @@ def convert_v5_need_once(input_text: str) -> str:
 ProcessPath = os.getcwd()
 
 
-def convert_conjugate(input_text: str) -> str:
+def convert_conjugate(input_text: str) -> list:
     """convert a verb conjugation to basic form.
         还原动词的活用变形
 
@@ -153,126 +142,172 @@ def convert_conjugate(input_text: str) -> str:
         input_text (str): A String containing the conjugation.
 
     Returns:
-        str: The text with conjugation converted to the basic form.
+        list: The list with conjugation converted to the basic form.
     """
     # 请注意以下4条规则指的是不含动词未活用时原型词尾假名规律
-    v1_last_letter = set("、ずよぬ")
-    v5_last_letter = set("わえおがきぎげこごしにねのばびべぼめもり")
+    v1_last_letter = set("、よぬ")
+    v5_last_letter = set("わえおがきぎげこごにねのばびべぼめもり")
+    # 这些词尾假名只可能来自サ変动词活用
+    sahen_last_letter = set("じぜ")
     adj_last_letter = set("くうす")
+    v1_sahen_last_letter = set("ず")
+    v5_sahen_last_letter = set("し")
     # 这些词尾假名只可能来自一段、五段的动词活用
-    v1_v5_last_letter = set("せたちてとなまられろ")
+    v1_v5_last_letter = set("たちてとなまられろ")
+    # 这些词尾假名只可能来自一段、五段、サ変的动词活用
+    v1_v5_sahen_last_letter = set("せ")
     # 这些词尾假名只可能来自形容词、五段的动词活用
-    adj_v5_last_letter = set("かけみそ")
+    adj_v5_last_letter = set("かけみ")
 
-    # FIXME  不使用 global
-    global process_output_list, INPUT_LAST_LETTER
-    process_output_list = []  # 保留查询的结果
-    search_index(input_text)  # 查看是否收录在词典中
-    INPUT_LAST_LETTER = input_text.replace("\n", "")[-1]
-    process_text = input_text + "る"  # 一段动词的连用形1
-    search_index(process_text)
-    if INPUT_LAST_LETTER in v1_last_letter:
-        print("词尾假名是：" + INPUT_LAST_LETTER + "有可能是一段动词")
-        process_text = input_text[0:-1] + "る"
-        search_index(process_text)
-    elif INPUT_LAST_LETTER in v5_last_letter:
-        print("词尾假名是：" + INPUT_LAST_LETTER + "有可能是五段动词")
-        process_text = convert_v5_need_once(input_text)
-        search_index(process_text)
-    elif INPUT_LAST_LETTER in adj_last_letter:
-        print("词尾假名是：" + INPUT_LAST_LETTER + "有可能是形容词")
-        process_text = input_text[0:-1] + "い"
-        search_index(process_text)
-    elif INPUT_LAST_LETTER in adj_v5_last_letter:
-        print("词尾假名是：" + INPUT_LAST_LETTER + "有可能是形容词，也有可能是五段动词")
-        process_text = input_text[0:-1] + "い"
-        search_index(process_text)
-        process_text = convert_v5(input_text)
-        search_index(process_text)
-    elif INPUT_LAST_LETTER in v1_v5_last_letter:
+    input_stem = input_text[0:-1]
+    input_last_letter = input_text[-1]
+
+    process_output_list = []
+
+    # 本程序的 input_stem 概念对应的不是一段动词语法意义上的词干
+    # 今日は、寿司を**食べ**に銀座に行いきます。
+    process_text = input_text + "る"
+    process_output_list.append(process_text)
+    if input_last_letter in v1_last_letter:
+        print("词尾假名是：" + input_last_letter + "有可能是一段动词")
+        process_text = input_stem + "る"
+        process_output_list.append(process_text)
+    elif input_last_letter in v5_last_letter:
+        print("词尾假名是：" + input_last_letter + "有可能是五段动词")
+        process_text = convert_v5_need_once(input_stem, input_last_letter)
+        process_output_list.append(process_text)
+    elif input_last_letter in adj_last_letter:
+        print("词尾假名是：" + input_last_letter + "有可能是形容词")
+        process_text = input_stem + "い"
+        process_output_list.append(process_text)
+    elif input_last_letter in sahen_last_letter:
+        print("词尾假名是：" + input_last_letter + "有可能是サ変")
+        process_text = input_stem + "ずる"
+        process_output_list.append(process_text)
+        process_text = input_stem + "す"
+        process_output_list.append(process_text)
+        process_text = input_stem + "ず"
+        process_output_list.append(process_text)
+    elif input_last_letter in adj_v5_last_letter:
+        print("词尾假名是：" + input_last_letter + "有可能是形容词，也有可能是五段动词")
+        process_text = input_stem + "い"
+        process_output_list.append(process_text)
+        process_text = convert_v5(input_text, input_stem, input_last_letter)
+        process_output_list.append(process_text)
+    elif input_last_letter in v1_v5_last_letter:
         print(
-            "词尾假名是：" + INPUT_LAST_LETTER + "有可能是一段动词，也有可能是五段动词"
+            "词尾假名是：" + input_last_letter + "有可能是一段动词，也有可能是五段动词"
         )
-        process_text = input_text[0:-1] + "る"
-        search_index(process_text)
-        process_text = convert_v5(input_text)
-        search_index(process_text)
-    elif INPUT_LAST_LETTER == "っ":
-        print("词尾假名是：" + INPUT_LAST_LETTER + "有可能是五段动词")
-        process_text = input_text[0:-1] + "る"
-        search_index(process_text)
-        process_text = input_text[0:-1] + "つ"
-        search_index(process_text)
-        process_text = input_text[0:-1] + "う"
-        search_index(process_text)
+        process_text = input_stem + "る"
+        process_output_list.append(process_text)
+        process_text = convert_v5(input_text, input_stem, input_last_letter)
+        process_output_list.append(process_text)
+    elif input_last_letter in v1_sahen_last_letter:
+        print("词尾假名是：" + input_last_letter + "有可能是一段动词，也有可能是サ変")
+        process_text = input_stem + "る"
+        process_output_list.append(process_text)
+        process_text = input_stem + "ずる"
+        process_output_list.append(process_text)
+    elif input_last_letter in v5_sahen_last_letter:
+        print("词尾假名是：" + input_last_letter + "有可能是五段动词，也有可能是サ変")
+        process_text = convert_v5_need_once(input_stem, input_last_letter)
+        process_output_list.append(process_text)
+        process_text = input_stem + "する"
+        process_output_list.append(process_text)
+    elif input_last_letter in v1_v5_sahen_last_letter:
+        print(
+            "词尾假名是："
+            + input_last_letter
+            + "有可能是一段动词，也有可能是五段动词，也有可能是サ変"
+        )
+        process_text = input_stem + "る"
+        process_output_list.append(process_text)
+        process_text = convert_v5(input_text, input_stem, input_last_letter)
+        process_output_list.append(process_text)
+        process_text = input_stem + "する"
+        process_output_list.append(process_text)
+    elif input_last_letter == "そ":
+        print("词尾假名是：" + input_last_letter + "有可能是五段动词")
+        process_text = input_stem + "い"
+        process_output_list.append(process_text)
+        process_text = input_stem + "す"
+        process_output_list.append(process_text)
+        process_text = input_stem + "する"
+        process_output_list.append(process_text)
+    elif input_last_letter == "っ":
+        print("词尾假名是：" + input_last_letter + "有可能是五段动词")
+        process_text = input_stem + "る"
+        process_output_list.append(process_text)
+        process_text = input_stem + "つ"
+        process_output_list.append(process_text)
+        process_text = input_stem + "う"
+        process_output_list.append(process_text)
         if input_text == "行っ":
             process_output_list.append("行く")
-            search_index(process_text)
-            process_output_list.append(input_text)
-    elif INPUT_LAST_LETTER == "さ":
+            process_output_list.append(process_text)
+    elif input_last_letter == "さ":
         print(
             "词尾假名是："
-            + INPUT_LAST_LETTER
+            + input_last_letter
             + "有可能是形容词，也有可能是五段动词，也有可能是一段动词"
         )
-        process_text = input_text[0:-1] + "い"
-        search_index(process_text)
-        process_text = input_text[0:-1] + "す"
-        search_index(process_text)
-        process_text = input_text[0:-1] + input_text[-1].replace(
-            INPUT_LAST_LETTER, "る"
-        )
-        search_index(process_text)
-    elif INPUT_LAST_LETTER == "ん":
+        process_text = input_stem + "い"
+        process_output_list.append(process_text)
+        process_text = input_stem + "す"
+        process_output_list.append(process_text)
+        process_text = input_stem + input_text[-1].replace(input_last_letter, "る")
+        process_output_list.append(process_text)
+        process_text = input_stem + "する"
+        process_output_list.append(process_text)
+    elif input_last_letter == "ん":
         print(
-            "词尾假名是：" + INPUT_LAST_LETTER + "有可能是一段动词，也有可能是五段动词"
+            "词尾假名是：" + input_last_letter + "有可能是一段动词，也有可能是五段动词"
         )
-        process_text = input_text[0:-1] + "む"
-        search_index(process_text)
-        process_text = input_text[0:-1] + "ぶ"
-        search_index(process_text)
-        process_text = input_text[0:-1] + "ぬ"
-        search_index(process_text)
-        process_text = input_text[0:-1] + "る"
-        search_index(process_text)
-    elif INPUT_LAST_LETTER == "い":
+        process_text = input_stem + "む"
+        process_output_list.append(process_text)
+        process_text = input_stem + "ぶ"
+        process_output_list.append(process_text)
+        process_text = input_stem + "ぬ"
+        process_output_list.append(process_text)
+        process_text = input_stem + "る"
+        process_output_list.append(process_text)
+    elif input_last_letter == "い":
         print(
             "词尾假名是："
-            + INPUT_LAST_LETTER
+            + input_last_letter
             + "有可能是五段动词活用，也有可能是辞書形"
         )
-        process_text = input_text[0:-1] + "う"
-        search_index(process_text)
-        process_text = input_text[0:-1] + "く"
-        search_index(process_text)
-        process_text = input_text[0:-1] + "ぐ"
-        search_index(process_text)
-    elif INPUT_LAST_LETTER == "ゃ":
-        print("词尾假名是：" + INPUT_LAST_LETTER + "有可能是一段动词")
+        process_text = input_stem + "う"
+        process_output_list.append(process_text)
+        process_text = input_stem + "く"
+        process_output_list.append(process_text)
+        process_text = input_stem + "ぐ"
+        process_output_list.append(process_text)
+    elif input_last_letter == "ゃ":
+        print("词尾假名是：" + input_last_letter + "有可能是一段动词")
         process_text = input_text[0:-2] + "る"
-        search_index(process_text)
+        process_output_list.append(process_text)
     else:
+        # 不是动词活用出现的非辞書型
         print("词尾假名出现例外情况！" + input_text)
         process_output_list.append(input_text)
-    process_output_list.append(input_text)  # 任何情况下都返回复制的值，便于手动修改
+
+    # 将输入的字符串作为最后一个结果返回
+    # 因为输入的字符串可能就是正确的辞書型
+    if input_text not in process_output_list:
+        process_output_list.append(input_text)
 
     # 删除其中的重复值，只保留第一次的结果
-    output_list = []
+    output_list: List[str] = []
     for i in process_output_list:
         if i not in output_list:
-            i = convert_orthography(i)
-            output_list.append(i)
+            orthography_text = convert_orthography(i)
+            if orthography_text is not None:
+                output_list.extend(set(orthography_text))
 
-    # 注意，直接使用join遇到数字时会报错，但通过剪贴板获取的数字会被转为字符串
-    output_text = "\n".join(output_list)
-    return output_text
+    # 将输入的字符串作为最后一个结果返回
+    # 方便用户在程序无法推导出正确结果时手动编辑
+    if input_text not in output_list:
+        output_list.append(input_text)
 
-
-INPUT_TEXT = "歩く"
-
-INPUT_TEXT = preprocess(INPUT_TEXT)
-
-OUTPUT_TEXT = convert_conjugate(INPUT_TEXT)
-print(OUTPUT_TEXT)
-END_TIME = time.perf_counter()
-print(f"耗时:{round((END_TIME - START_TIME) * 1000, 3)}毫秒")
+    return output_list
