e7604793f2272cf182369e7ceeabccee8f5b6641
noheartpen <3104408599@qq.com>
2024-04-20 20:50:02 +0900

    refactor: 适配macOS打包

--- v2/main.py
+++ v2/main.py
@@ -1,9 +1,21 @@
+import glob
+import logging
 import os
 import re
+import sys
+import time
 
-ProessPath = os.getcwd()
-# 切换到当前文件所在的路径ß
-os.chdir(ProessPath)
+logging.basicConfig(
+    handlers=[
+        logging.FileHandler(
+            f"{time.strftime('%Y-%m-%d', time.localtime()) }.log", encoding="utf-8"
+        ),
+        logging.StreamHandler(sys.stderr),
+    ],
+    level=logging.DEBUG,
+    format="%(asctime)s %(filename)s %(levelname)s %(message)s",
+    datefmt="%a %d %b %Y %H:%M:%S",
+)
 
 INDEX_SET = set()
 
@@ -671,8 +683,8 @@ def InflectItidan():
 
 
 def InflectNoun():
-    with open(r"index\noun.txt", "r", encoding="utf-8") as InputFile, open(
-        r"process\noun.txt", "w", encoding="utf-8"
+    with open(r"index/noun.txt", "r", encoding="utf-8") as InputFile, open(
+        r"process/noun.txt", "w", encoding="utf-8"
     ) as OutputFile:
         for line in InputFile:
             line = line.replace("\n", "")
@@ -705,50 +717,69 @@ def InflectProcess():
     InflectNoun()
 
 
-def PackProcess():
+def pack2mdx():
+    """使用 mdict-utils 打包为mdx文件"""
+    process_pack_file, release_pack_file = check_pack_file()
+
+    release_path = os.path.join(CURRENT_PATH, "release_pub")
+    title_file = os.path.join(release_path, "title.html")
+    description_file = os.path.join(release_path, "description.html")
+    mdx_file = os.path.join(release_path, "NonJishoKei.mdx")
+    pack_config = f"--title {title_file} --description {description_file}"
+    pack_cmd = f"mdict {pack_config} -a {release_pack_file} {mdx_file}"
+    # 打包
+    os.system(pack_cmd)
+    # 删除过程文件
+    os.remove(process_pack_file)
+    os.remove(release_pack_file)
+
+
+def check_pack_file():
+    """检查打包文件"""
     # 合并所有词条
-    os.system(r"copy process\*.txt processpack.txt")
-    ProcessPackFile = r"processpack.txt"
-    ReleasePackFile = r"process\releasepack.txt"
+    source_dir = os.path.join(CURRENT_PATH, "process")
+    process_pack_file = os.path.join(source_dir, "pack_process.txt")
+    release_pack_file = os.path.join(source_dir, "pack_release.txt")
+    # 获取所有 .txt 文件的路径
+    source_files = glob.glob(os.path.join(source_dir, "*.txt"))
+    for file in source_files:
+        with open(file, "r", encoding="utf-8") as source_file, open(
+            process_pack_file, "a", encoding="utf-8"
+        ) as pack_process_file:
+            pack_process_file.write(source_file.read())
 
     # 清理重复行
-    FileContextSet = set()
-    with open(ProcessPackFile, "r", encoding="utf-8") as InputFile, open(
-        ReleasePackFile, "w", encoding="utf-8"
-    ) as OutputFile:
-        Context = InputFile.read()
-        Context = Context.replace("\n", "")
-        Context = Context.replace("</>", "</>\n")
-        Line = Context.split("\n")
-        for item in Line:
-            FileContextSet.add(item)
-        OutputFileContext = list(FileContextSet)
+    process_pack_set = set()
+    with open(process_pack_file, "r", encoding="utf-8") as input_file, open(
+        release_pack_file, "w", encoding="utf-8"
+    ) as output_file:
+        process_pack_file_text = input_file.read()
+        # 将需要打包的文件按照一个词条占一行的原则，删除那些完全一样的行
+        process_pack_file_text = process_pack_file_text.replace("\n", "")
+        process_pack_file_text = process_pack_file_text.replace("</>", "</>\n")
+        process_pack_file_lines = process_pack_file_text.split("\n")
+        for line in process_pack_file_lines:
+            process_pack_set.add(line)
+        release_pack_list = list(process_pack_set)
 
-        # 生成打包文件
-        error_line_reg = re.compile(
-            r'^<section class="description">'
-        )  # 不符合mdx文件格式规范会导致打包时报错
-        for item in OutputFileContext:
-            if re.search(error_line_reg, item) != None:
-                print("出现空行异常，请注意检查！\n" + item)
+        # 不符合mdx文件格式规范会导致打包时报错
+        error_line_reg = re.compile(r'^<section class="description">')
+        for line in release_pack_list:
+            if re.search(error_line_reg, line) is not None:
+                logging.critical("%s", line)
                 continue
-            item = item.replace(
+            line = line.replace(
                 '<section class="description">', '\n<section class="description">'
             )
-            if "</a>" in item:
-                item = item.replace("</a>", "</a>\n")
-            item = item.replace("</section></>", "</section>\n</>\n")
-            OutputFile.writelines(item)
-    # 打包
-    os.system(
-        r"mdict --title release_pub\title.html --description release_pub\description.html -a process\releasepack.txt release_pub\NonJishoKei.mdx"
-    )
-    # 删除过程文件
-    os.remove(ProcessPackFile)
-    os.remove(ReleasePackFile)
+            if "</a>" in line:
+                line = line.replace("</a>", "</a>\n")
+            line = line.replace("</section></>", "</section>\n</>\n")
+            output_file.writelines(line)
+    return process_pack_file, release_pack_file
 
 
 CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
+os.chdir(CURRENT_PATH)
 init_index_file(os.path.join(CURRENT_PATH, r"index/index.txt"))
 InflectProcess()
-PackProcess()
+pack2mdx()
